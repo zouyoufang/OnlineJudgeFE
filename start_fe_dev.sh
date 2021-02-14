dd5cc0b29897bd3449adf9666f7b8aae09e5ed71
zouyoufang <zouyoufang@live.com>
2021-02-14 18:28:37 +0800

    adjust package

---
+++ start_fe_dev.sh
@@ -0,0 +1,12 @@
+# FE port
+export PORT=12079 
+
+#export NODE_ENV=development
+export NODE_ENV=production
+
+# backend port
+export TARGET=http://127.0.0.1:12080
+
+#npm run prod &>npm_fe.log &
+npm run dev 
+
